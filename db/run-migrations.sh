7f715e161752027958b2d850ea4f63d48f9a150d
jay-web <jay.developer12@gmail.com>
2026-04-24 15:48:45 +0530

    feat(ayd): add hybrid local-dev mode with local pgvector postgres and local ingestion worker

--- db/run-migrations.sh
+++ db/run-migrations.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/001_create_workspaces.sql
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/002_create_workspace_memberships.sql
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/003_create_documents.sql
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/004_create_document_chunks.sql
+export PGPASSWORD="$DB_PASSWORD"
+
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/001_create_workspaces.sql
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/002_create_workspace_memberships.sql
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/003_create_documents.sql
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f db/migrations/004_create_document_chunks.sql
